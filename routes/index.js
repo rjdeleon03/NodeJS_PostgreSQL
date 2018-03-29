f721170f20966cc69a25388461470b360e8ae3a2
rjdeleon <ricardopablo.deleon@gmail.com>
2018-03-29 10:13:24 +0800

    Implement adding of patient on form submit

--- routes/index.js
+++ routes/index.js
@@ -8,28 +8,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/db', function (req, res, next) {  
 
-  var dbPool = req.app.get("dbPool");
-  dbPool.connect(function(err,client,done) {
-     if(err){
-         console.log("not able to get connection "+ err);
-         res.status(400).send(err);
-     } 
-     client.query("INSERT INTO patients(name, age, location) \
-                    VALUES('Clara', '4', 'Manila')", 
-                    function(err,result) {
-         done(); // closing the connection;
-         if(err){
-             console.log(err);
-             res.status(400).send(err);
-         }
-         if (result) {
-          res.status(200).send(result);
-         } else {
-           res.status(200).send("NO DB DATA FOUND!!!");
-         }
-     });
-  });
-  dbPool.end();
+ 
 });
 
 module.exports = router;
--- routes/patients.js
+++ routes/patients.js
@@ -14,7 +14,30 @@ router.get('/new', function(req, res, next) {
 /* POST new patient */
 router.post('/', function(req, res, next) {
     console.log(req.body);
-  res.render("patients/index");
+
+    var dbPool = req.app.get("dbPool");
+    dbPool.connect(function(err,client,done) {
+       if(err){
+           console.log("not able to get connection "+ err);
+           res.status(400).send(err);
+       } 
+       client.query("INSERT INTO patients(name, age, location) \
+                      VALUES($1, $2, $3)",
+                      [req.body.name, req.body.age, req.body.location], 
+                      function(err,result) {
+           done(); // closing the connection;
+           if(err){
+               console.log(err);
+               res.status(400).send(err);
+           }
+           if (result) {
+            res.status(200).send(result);
+           } else {
+             res.status(200).send("NO DB DATA FOUND!!!");
+           }
+       });
+    });
+    dbPool.end();
 });
 
 module.exports = router;
